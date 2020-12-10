93d619f1eb39b23d69e6a672bc3684b8ed031e29
clauskonrad <claus.konrad@gmx.de>
2020-12-10 11:29:30 +0100

    Update Probezeit nicht bestanden.sql

--- SQLSkripte/Probezeit nicht bestanden.sql
+++ SQLSkripte/Probezeit nicht bestanden.sql
@@ -1,5 +1,8 @@
-SELECT Schueler.Name, Schueler.Vorname, Klasse.Bezeichnung
+use diNo;
+
+SELECT Klasse.Bezeichnung, Schueler.Name, Schueler.Vorname, Vorkommnis.Bemerkung, Schueler.Geburtsdatum
 FROM Schueler, Klasse, Vorkommnis
 WHERE Schueler.KlasseId = Klasse.Id
 AND Schueler.Id = Vorkommnis.SchuelerId
-AND Vorkommnis.Art = 1
+AND Vorkommnis.Art = 11
+order by Bezeichnung,Name
