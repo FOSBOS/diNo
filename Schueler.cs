cec5fb738e44fef3f69b23f1b1bda2abb9a5ee2f
clauskonrad <claus.konrad@gmx.de>
2022-08-19 12:28:17 +0200

    Schulliste

--- Schueler.cs
+++ Schueler.cs
@@ -880,8 +880,9 @@ namespace diNo
     public static string GetSchulname(int schulnummer)
     {
       // WinSV liefert 7777 für außerbay. Schule?
-      bool b = schulnummer!=7777 && schulenInBayern.ContainsKey(schulnummer);
-      return b ? schulenInBayern[schulnummer] : "";      
+      //bool b = schulnummer!=7777 && schulenInBayern.ContainsKey(schulnummer);
+      //return b ? schulenInBayern[schulnummer] : "";
+      return schulenInBayern[schulnummer];
     }
   }
 
