6cf29de863ccb74c11209b38a3c7abebabc621e1
clauskonrad <claus.konrad@gmx.de>
2022-12-09 10:12:14 +0100

    SQL Skript

--- SQLSkripte/Probezeit nicht bestanden.sql
+++ SQLSkripte/Probezeit nicht bestanden.sql
@@ -1,8 +1,8 @@
 use diNo;
 
-SELECT Klasse.Bezeichnung, Schueler.Name, Schueler.Vorname, Vorkommnis.Bemerkung, Schueler.Geburtsdatum
+SELECT Klasse.Bezeichnung, Schueler.Name, Schueler.Rufname, Vorkommnis.Bemerkung, Schueler.Geburtsdatum
 FROM Schueler, Klasse, Vorkommnis
 WHERE Schueler.KlasseId = Klasse.Id
 AND Schueler.Id = Vorkommnis.SchuelerId
-AND Vorkommnis.Art = 11 OR Vorkommnis.Art = 17 --Probezeit nicht bestanden oder verlängert
+AND Vorkommnis.Art = 11 --OR Vorkommnis.Art = 17 --Probezeit nicht bestanden oder verlängert
 order by Bezeichnung,Name
--- SQLSkripte/Update.sql
+++ SQLSkripte/Update.sql
@@ -4,4 +4,3 @@ ALTER TABLE dbo.Schueler ADD
 	LRSZuschlagMin int NOT NULL DEFAULT 0,
 	LRSZuschlagMax int NOT NULL DEFAULT 0
 	
---ALTER TABLE dbo.Schueler DROP COLUMN SonderfallNur2Hj
