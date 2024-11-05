da00a03818736f190800c0aa5e9013a40bcf1c1f
clauskonrad <claus.konrad@gmx.de>
2024-11-05 12:05:42 +0100

    Umgang mit neuem Outlook

--- Klassenansicht.cs
+++ Klassenansicht.cs
@@ -354,24 +354,9 @@ namespace diNo
     private void btnMail_Click(object sender, EventArgs e)
     {
       var obj = treeListView1.SelectedObjects;
-      if (obj == null) MessageBox.Show("Bitte einen Schüler oder eine Klasse auswählen.", "diNo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); ;
-      try
-      { 
-        if (obj[0] is Klasse)
-        {
-          Klasse k = (Klasse)obj[0];
-          OpenOutlook.NewMail(k);
-        }          
-        else 
-        {
-          Schueler s = (Schueler)obj[0];
-          OpenOutlook.NewMail(s.Kurse, s.VornameName + ", " + s.getKlasse.Bezeichnung);
-        }
-      }
-      catch (Exception ex)
-      {
-        MessageBox.Show("Öffnen von Outlook war nicht möglich.\n" + ex.ToString(), "diNo", MessageBoxButtons.OK, MessageBoxIcon.Error);
-      }
+      if (obj == null) MessageBox.Show("Bitte einen Schüler oder eine Klasse auswählen.", "diNo", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+      else
+        OpenOutlook.BetroffeneLehrer(obj[0]);
     }
   }
 }
--- OpenOutlook.cs
+++ OpenOutlook.cs
@@ -6,13 +6,14 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using Outlook = Microsoft.Office.Interop.Outlook; // Verweis hinzufügen -> COM -> Microsoft Outlook Object Library
+using System.Windows.Forms;
+using System.ComponentModel;
 
 namespace diNo
 {
   public static class OpenOutlook
   {
 
-    // es fehlt der Test, ob Outlook offen ist:
     // https://learn.microsoft.com/de-de/office/client-developer/outlook/pia/how-to-get-and-log-on-to-an-instance-of-outlook
 
     public static Outlook.Application GetOutlookObject()
@@ -23,18 +24,18 @@ namespace diNo
       // Check whether there is an Outlook process running.
       if (Process.GetProcessesByName("OUTLOOK").Count() > 0)
       {
-
         // If so, use the GetActiveObject method to obtain the process and cast it to an Application object.
         application = Marshal.GetActiveObject("Outlook.Application") as Outlook.Application;
       }
       else
       {
-
+        // dauert lange, wenn das neue Outlook installiert ist --> deshalb auf NullPointer-Exception laufen lassen 
+      /*
         // If not, create a new instance of Outlook and sign in to the default profile.
         application = new Outlook.Application();
         Outlook.NameSpace nameSpace = application.GetNamespace("MAPI");
         nameSpace.Logon("", "", Missing.Value, Missing.Value);
-        nameSpace = null;
+        nameSpace = null;*/
       }
 
       // Return the Outlook Application object.
@@ -59,20 +60,44 @@ namespace diNo
       
       oMailItem.Display(false);     
     }
-
-    public static void NewMail(Klasse klasse)
+   
+    public static void BetroffeneLehrer(Object obj)
     {
       List<Kurs> kurse = new List<Kurs>();
-      foreach (Kurs k in klasse.Kurse)
-      {
-        FachTyp typ = k.getFach.Typ;
-        string kue = k.getFach.Kuerzel;
-        if (typ==FachTyp.Allgemein && kue!="K" && kue != "Ev" && kue != "Eth" || typ == FachTyp.Profilfach)
+      try
+      {        
+        if (obj is Klasse)
         {
-          kurse.Add(k);
+          
+          Klasse klasse = (Klasse)obj;
+          foreach (Kurs k in klasse.Kurse)
+          {
+            FachTyp typ = k.getFach.Typ;
+            string kue = k.getFach.Kuerzel;
+            if (typ == FachTyp.Allgemein && kue != "K" && kue != "Ev" && kue != "Eth" || typ == FachTyp.Profilfach)
+            {
+              kurse.Add(k);
+            }
+          }
+          NewMail(kurse, "Klasse " + klasse.Bezeichnung);
+        }
+        else
+        {
+          Schueler s = (Schueler)obj;
+          kurse = s.Kurse;
+          NewMail(s.Kurse, s.VornameName + ", " + s.getKlasse.Bezeichnung);
         }
       }
-      NewMail(kurse, "Klasse " + klasse.Bezeichnung);
+      catch (Exception ex)
+      {
+        string erg="";
+        foreach (Kurs k in kurse)
+        {
+          erg += k.getLehrer.Data.EMail + "; ";          
+        }
+        Clipboard.SetText(erg);
+        MessageBox.Show("Öffnen von Outlook war nicht möglich.\nErstellen Sie ein neues Mail und fügen Sie über Strg+V die Empfänger ein.", "diNo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+      }
     }
   }
 }
