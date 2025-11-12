e689d05436a90402e39ba564e3a0f04da6a02a79
Claus <dino-copy@bszke.de>
2025-11-12 11:29:12 +0100

    Update Update.sql

--- SQLSkripte/Update.sql
+++ SQLSkripte/Update.sql
@@ -1,4 +1,6 @@
 use diNo;
 
-ALTER TABLE dbo.Schueler ADD
-	MailSchule NVARCHAR(50) NULL DEFAULT ''
+
+-- bereits im Produktivsystem eingespielte Änderungen:
+-- ALTER TABLE Schueler ADD CONSTRAINT chk_schueler_geschlecht CHECK (Geschlecht IN ('M','W','D'));
+-- ALTER TABLE dbo.Schueler ADD MailSchule NVARCHAR(50) NULL DEFAULT ''
