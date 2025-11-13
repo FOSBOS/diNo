587439eb2e60427dd763c96bae6c47e1f6d839b8
clauskonrad <claus.konrad@gmx.de>
2025-11-13 09:56:00 +0100

    Update Update.sql

--- SQLSkripte/Update.sql
+++ SQLSkripte/Update.sql
@@ -1,7 +1,8 @@
 use diNo;
 
--- bereits im Produktivsystem eingespielte Änderungen:
--- ALTER TABLE Schueler ADD CONSTRAINT chk_schueler_geschlecht CHECK (Geschlecht IN ('M','W','D'));
--- ALTER TABLE KlasseKurs ADD PRIMARY KEY (KursId,KlasseId)
+insert into GlobaleStrings values (29,'MailAdresseTest','stefan.hein@fos-sonthofen-cloud.de')
+ALTER TABLE Schueler ADD CONSTRAINT chk_schueler_geschlecht CHECK (Geschlecht IN ('M','W','D'));
 
+-- bereits im Produktivsystem eingespielte Änderungen:
+-- ALTER TABLE KlasseKurs ADD PRIMARY KEY (KursId,KlasseId)
 -- ALTER TABLE dbo.Schueler ADD MailSchule NVARCHAR(50) NULL DEFAULT ''
