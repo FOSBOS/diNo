f87a7765a38996ab25494cf0f890af5ee55dd80e
Claus <dino-copy@bszke.de>
2026-06-24 12:32:02 +0200

    Update SchuelerMitAndereFremdspr2.sql

--- SQLSkripte/SchuelerMitAndereFremdspr2.sql
+++ SQLSkripte/SchuelerMitAndereFremdspr2.sql
@@ -1,13 +1,20 @@
 use diNo;
 
-SELECT Id
-      ,[Name]
-      ,[KlasseId]
-      ,[Rufname]
-      ,[AndereFremdspr2Note]
-      ,[AndereFremdspr2Text]
-      ,[AndereFremdspr2Art]
-	  ,[AndereFremdspr2Fach]
-	  , Status
-  FROM [diNo].[dbo].[Schueler]
-  where AndereFremdspr2Note is not null and Status=0
+SELECT 
+	Klasse.Bezeichnung
+	  ,Name     
+      ,Rufname
+      ,AndereFremdspr2Note
+	  ,Fach.Bezeichnung	  
+  FROM Schueler,Klasse,Fach
+  where Klasse.Id=Schueler.KlasseId and
+  Fach.Id = Schueler.AndereFremdspr2Fach and
+  AndereFremdspr2Note is not null and Status=0
+  and Klasse.JgStufe=13
+  and Schueler.Id not in
+( select SchuelerId
+  from Vorkommnis
+  where Art>20 and Art<32 or Art=47
+)
+  order by Klasse.Bezeichnung,Name
+
